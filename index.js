65e8164b4fb97f715b73a4595413069636bdd5c4
Martín Matayoshi <kamikaizer@gmail.com>
2020-03-22 22:14:29 -0300

    tarea

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-const elementos = ["papel", "piedra", "tijera"]
-alert('comenzamos')
+var usuario = prompt (`Juguemos cachipun. Elije entre papel, piedra, tijera`)
+var opciones = ["piedra","papel","tijera"]
+
+functionJuego(){
+
+}
+
+console.log(elementos)
